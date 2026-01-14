382e0ef4bab15ff696bb5506e5033356964a9f0d
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-01-14 10:30:13 +0000

    Cria a classe LoginViewModel.

---
+++ TaskGX/ViewModel/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TaskGX.Ferramentas;
+
+namespace TaskGX.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Nome { get; set; }
+        public string SenhaHash { get; set; }
+        public ICommand ComandoLogin {get; }
+
+        public LoginViewModel()
+        {
+            ComandoLogin = new RelayCommand(Login);
+        }
+
+        public void Login() { }
+    }
+}
